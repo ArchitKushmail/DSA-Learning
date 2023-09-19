2eae8defcdce227a2b1be10ee3e87b90a45ab2d3
ArchitKushmail <architkushmail1@gmail.com>
2023-09-19 11:55:24 +0530

    checkPalindrome

---
+++ Recursion/Class 4/checkPalindrome.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool checkPalindrome(string &str,int s,int e){
+    if(s>e){
+        return true;
+    }
+    if(str[s]!=str[e]){
+        return false;
+    }
+    s++;
+    e--;
+    return checkPalindrome(str,s,e);
+}
+int main(){
+    
+    string str = "apa";
+    cout<<endl;
+    bool ans = checkPalindrome(str,0,str.length()-1);
+    cout<<ans;
+    return 0;
+
+}
